0321c5af7a4323d8ca9e12e314ff750d1f716333
Matthew <matthew.fontana@streamr.com>
2020-09-10 17:21:49 +0300

    no message

--- build/config/prod.env.js
+++ build/config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  SWAGGER_PATH: '"https://cdn.streamr.com/swagger.json"'
+  SWAGGER_PATH: '"https://gitcdn.xyz/cdn/streamr-dev/engine-and-editor/5e4d5dc22c5ca60ec11bc6fc034e0d2cd72e451d/docs/swagger.json"'
 }
