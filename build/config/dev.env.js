f51d1c639c242786f6709e0bc62c89c50a6e0c09
Matthew <matthew.fontana@streamr.com>
2021-03-22 10:48:32 +0100

    move swagger hosting to raw.githubusercontent

--- build/config/dev.env.js
+++ build/config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SWAGGER_PATH: '"https://gitcdn.xyz/cdn/streamr-dev/engine-and-editor/5e4d5dc22c5ca60ec11bc6fc034e0d2cd72e451d/docs/swagger.json"'
+  SWAGGER_PATH: '"https://raw.githubusercontent.com/streamr-dev/engine-and-editor/master/docs/swagger.json"'
 })
