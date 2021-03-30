7ed91ae705eb033a1b8cbddf94ab4819f0cf01bd
Matthew <matthew.fontana@streamr.com>
2021-03-30 14:52:26 +0200

    github pages swagger

--- build/config/prod.env.js
+++ build/config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  SWAGGER_PATH: '"https://raw.githubusercontent.com/streamr-dev/engine-and-editor/master/docs/swagger.json"'
+  SWAGGER_PATH: '"https://streamr-dev.github.io/engine-and-editor/swagger.json"'
 }
